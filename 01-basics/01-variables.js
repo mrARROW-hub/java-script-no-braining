c8ab9d0cb59664d5cc45fb8f6123e4e39764528c
ARROW <rishabbhkumar0002@gmail.com>
2026-01-17 17:29:19 +0000

    01-basics/01-variables.js

---
+++ 01-basics/01-variables.js
@@ -0,0 +1,7 @@
+console.log("rishabh")
+const accountId="21212121"
+let accountPassword="02020202"
+let accountEmail="rishabh@google.com"
+let accountState="punjab"
+let accountCity;
+console.table([accountId,accountPassword,accountEmail,accountState,accountCity])
