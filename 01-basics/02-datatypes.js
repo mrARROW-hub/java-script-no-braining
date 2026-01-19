065dbe23702e38ffbf2ea85564cbc309d991cdd7
ARROW <rishabbhkumar0002@gmail.com>
2026-01-19 16:56:09 +0000

    01-basics/02-datatypes.js

--- 01-basics/02-datatypes.js
+++ 01-basics/02-datatypes.js
@@ -1,6 +1,6 @@
 "use strict"//to use a newer version of java script//
 
- alert( 3 + 3)//it will not work, because we are using node js, not a browser,so it will not support it here//
+alert( 3 + 3) //it will not work, because we are using node js, not a browser,so it will not support it here,hence remove this code to execute all the rest.//
 
 let name="jojo"
 console.log(typeof "jojo")
