79f42d19862305d25c90caa3ea8b33099d100093
ARROW <rishabbhkumar0002@gmail.com>
2026-01-22 15:12:50 +0000

    01-basics/03-conversionOperation.js

---
+++ 01-basics/03-conversionOperation.js
@@ -0,0 +1,10 @@
+let score = null//by writting the no. in double quotes, the type of score will be shown as the string.without them it will show numbers//
+console.log(typeof(score))//these sre the two types of finding type of//
+console.log(typeof score)
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber)//to convert string into number//
+
+console.log(valueInNumber)
+
+//********************************operations**********************************//
