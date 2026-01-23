280e43dfd029845001e09a9161d1803601f26d80
ARROW <rishabbhkumar0002@gmail.com>
2026-01-23 15:25:56 +0000

    01-basics/03-conversionOperation.js

--- 01-basics/03-conversionOperation.js
+++ 01-basics/03-conversionOperation.js
@@ -1,10 +1,27 @@
-let score = null//by writting the no. in double quotes, the type of score will be shown as the string.without them it will show numbers//
-console.log(typeof(score))//these sre the two types of finding type of//
-console.log(typeof score)
+let score = "33"//by writting the no. in double quotes, the type of score will be shown as the string.without them it will show numbers//
+//console.log(typeof(score))//these sre the two types of finding type of//
+//console.log(typeof score)
 
 let valueInNumber=Number(score)
-console.log(typeof valueInNumber)//to convert string into number//
+//console.log(typeof valueInNumber)//to convert string into number//
 
-console.log(valueInNumber)
+//console.log(valueInNumber)
 
-//********************************operations**********************************//
+//****************************operations**********************************//
+let value= 3
+let negValue=-value
+//console.log(negValue)
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2*2);
+//console.log(2**2); //these all are operations//
+//console.log(2**3);
+//console.log(2/3); 
+//console.log(2%3);
+
+// wow1="brawl"
+//let wow2="stars"
+//let wow3=wow1+wow2
+//console.log(wow3)
