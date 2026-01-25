e72ded979231db93a08dbc1e5c97294b2cd1c4f9
ARROW <rishabbhkumar0002@gmail.com>
2026-01-25 14:39:29 +0000

    04-comparision.js

--- 01-basics/02-datatypes.js
+++ 01-basics/02-datatypes.js
@@ -1,6 +1,8 @@
 "use strict"//to use a newer version of java script//
 
-alert( 3 + 3) //it will not work, because we are using node js, not a browser,so it will not support it here,hence remove this code to execute all the rest.//
+//alert( 3 + 3) //it will not work, because we are using node js, not a browser,so it will not support it here,hence remove this code to execute all the rest.//
 
-let name="jojo"
-console.log(typeof "jojo")
+//let name="jojo"
+//console.log(typeof "jojo")
+
+console.log( 2 > 1);
---
+++ 04-comparision.js
@@ -0,0 +1,5 @@
+//console.log( 2 > 1);
+//console.log( 2 < 1);
+//console.log( 2 >= 1);
+//console.log( 2 == 1);// these codes will compare the no. with each other//
+//console.log( 2 != 1);
