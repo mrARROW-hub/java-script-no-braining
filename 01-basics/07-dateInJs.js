e840dfa41910d706325308918718eeaa7be70423
ARROW <rishabbhkumar0002@gmail.com>
2026-02-09 15:00:36 +0000

    01-basics/07-dateInJs.js

---
+++ 01-basics/07-dateInJs.js
@@ -0,0 +1,14 @@
+//Dates
+
+let myDate = new Date();
+//console.log(myDate)
+//console.log(myDate.toDateString());
+//console.log(myDate.toISOString());
+//console.log(myDate.toJSON());
+//console.log(myDate.toLocaleDateString());//mainly used//
+//console.log(myDate.toLocaleString());//mainly used//
+//console.log(myDate.toLocaleTimeString());
+//console.log(myDate.toString());//mainly used//
+//console.log(myDate.toTimeString());
+//console.log(myDate.toUTCString());//mainly used//
+
