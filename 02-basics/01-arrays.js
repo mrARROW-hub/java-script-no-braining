050c3942b9652ce5994354597a4d6a43ef28ffc9
ARROW <rishabbhkumar0002@gmail.com>
2026-02-10 16:34:35 +0000

    02-basics/01-arrays.js

---
+++ 02-basics/01-arrays.js
@@ -0,0 +1,21 @@
+//Array
+
+const myArray = ["brawlstars", 0, 8, 9, 10];
+//console.log(myArray[0]);
+//console.log(myArray[1]);
+//console.log(myArray[2]);
+//console.log(myArray[3]);
+//console.log(myArray[4]);
+
+//Array methods//
+
+//myArray.push("lol");
+//myArray.pop();//push makes the last element given to the array remove//
+//console.log(myArray);
+
+//myArray.unshift(100);//adds the element at the start//
+//myArray.shift();//removes the first element//
+const newArray = myArray.join();
+console.log(myArray);
+console.log(newArray);//by joining the two arrays they adds up to form a string//
+console.log(typeof newArray);
